e59a49f661c20e527965e73f8482388ff2d843ef
Ikeno Sosuke <ikenosousuke@icloud.com>
2023-10-26 12:45:45 +0900

    no memo

--- src/components/firstStorage.js
+++ src/components/firstStorage.js
@@ -1,14 +1,14 @@
 
 const firstStorage = ()=>{
-    const contents = [{"title":"new memo","content":"contents was loaded"},{"title":"買い物","content":"にんじん、ジャガイモ、玉ねぎ"},{"title":"期末テスト","content":"数学、電気"},{"title":"new memo","content":""},{"title":"new memo", "content":""}]
-    if(!localStorage.getItem(0)){
+    const contents = [{"title":"new memo","content":"contents was loaded"},{"title":"買い物","content":"にんじん、ジャガイモ、玉ねぎ"},{"title":"期末テスト","content":"数学、電気"},{"title":"new memo","content":"none"},{"title":"new memo", "content":"none"}]
+    if(!localStorage.getItem(4)){
         let i = 0;
         for (i=0;i<contents.length;i++){
             localStorage.setItem(i,JSON.stringify(contents[i]))
         }
     }else{
         console.log("storage has value")
-        localStorage.clear()
+        // localStorage.clear()
     }
 }
 
